58e70b45a69a43d29cc63f39e776853d26b50471
asimimamabdullah <asimimamabdullah@gmail.com>
2022-11-26 22:56:35 +0500

    sorted messages

--- src/screens/ChatScreen.tsx
+++ src/screens/ChatScreen.tsx
@@ -7,33 +7,59 @@ import {
 } from "react-native";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { API, graphqlOperation } from "aws-amplify";
-import { getChatRoom } from "../graphql/queries";
+import { getChatRoom, listMessagesByChatRoom } from "../graphql/queries";
 import Message from "../components/ChatScreen/Message/Message";
 import bg from "../../assets/images/BG.png";
 import { messages } from "../../assets/data";
 import InputBox from "../components/ChatScreen/InputBox/InputBox";
 import { GraphQLResult } from "@aws-amplify/api-graphql";
-import { ChatRoom, GetChatRoomQuery } from "../API";
+import {
+	ChatRoom,
+	GetChatRoomQuery,
+	Message as MessageType,
+	ListMessagesByChatRoomQuery,
+} from "../API";
 
 const ChatScreen = () => {
 	const [chatRoom, setChatRoom] = useState<ChatRoom | null | undefined>(null);
+	const [messages, setMessages] = useState<Array<MessageType | null> | null>(
+		[],
+	);
 	const route = useRoute<any>();
 	const navigation = useNavigation();
 
 	const chatroomID = route.params.id;
 
+	// fetch chat room
 	useEffect(() => {
 		const chatrooms: Promise<GraphQLResult<any>> = API.graphql(
 			graphqlOperation(getChatRoom as GetChatRoomQuery, {
 				id: chatroomID,
 			}),
 		);
+		chatrooms.then((result: GraphQLResult<GetChatRoomQuery>) =>
+			setChatRoom(result.data?.getChatRoom as ChatRoom),
+		);
+	}, [chatroomID]);
+
+	useEffect(() => {
+		const chatrooms: Promise<GraphQLResult<any>> = API.graphql(
+			graphqlOperation(
+				listMessagesByChatRoom as ListMessagesByChatRoomQuery,
+				{
+					chatroomID,
+					sortDirection: "DESC",
+				},
+			),
+		);
 
 		// chatrooms.then((result) => setChatRoom(result.data.getChatRoom));
-		chatrooms.then((result: GraphQLResult<GetChatRoomQuery>) => {
-			setChatRoom(result.data?.getChatRoom as ChatRoom);
-		});
-	}, []);
+		chatrooms.then((result: GraphQLResult<ListMessagesByChatRoomQuery>) =>
+			setMessages(
+				result.data?.listMessagesByChatRoom?.items as Array<MessageType>,
+			),
+		);
+	}, [chatroomID]);
 
 	useEffect(() => {
 		navigation.setOptions({ title: route.params.name });
@@ -46,7 +72,7 @@ const ChatScreen = () => {
 	return (
 		<ImageBackground source={bg} style={{ ...styles.bg }}>
 			<FlatList
-				data={chatRoom.Messages?.items}
+				data={messages}
 				renderItem={({ item }) => <Message message={item} />}
 				style={styles.list}
 				inverted
